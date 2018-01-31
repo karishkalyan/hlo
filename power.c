10d30229ff74e12a758ced2da8983c2fe6b0b8eb
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-01-31 16:02:54 +0530

    power
    find the power of the number

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,m,p;
+printf("\n enter two numbers:");
+sacnf("%d",&n,&m);
+p=pow(n,m);
+printf ("\n power of the num %d is %d",n,p);
+getch();
+}
